ca44e4bb99f7158efa1acc077a9b9a86b677bead
Jimmy Vo <j.vo718@gmail.com>
2022-09-28 14:13:57 -0400

    v1 exponenent recursion

--- recursion_exercises.rb
+++ recursion_exercises.rb
@@ -20,3 +20,16 @@ end
 
 # p range_itr(1,5)
 # p range_rec(1,5)
+
+
+def exp_1(base,exp)
+   return 1 if exp == 0
+
+    base * exp_1(base,exp-1)
+end
+
+p exp_1(2,0)
+p exp_1(2,1)
+p exp_1(2,2)
+p exp_1(2,3)
+
